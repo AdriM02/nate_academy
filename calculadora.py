059d5503afbb156b7bc4910987e0b3546cb47cfc
AdriM02 <adrian.las.arana@gmail.com>
2018-01-17 17:27:39 +0100

    Añadiendo el archivo calculadora

---
+++ calculadora.py
@@ -0,0 +1,6 @@
+tipo_operacion = input ("¿Qué operación quieres realizar (multiplicar / sumar / restar)?")
+primer_numero = input ("Primer número: ")
+segundo_numero = input ("Segundo número: ")
+
+if tipo_operacion == ("multiplicar")
+    print (primer_numero * segundo_numero)
--- example_while.py
+++
@@ -1,7 +0,0 @@
-numero_inicial = 10
-
-while numero_inicial >= 0:
-    print(numero_inicial)
-    numero_inicial -= 1
-
-print("He terminadorl")
