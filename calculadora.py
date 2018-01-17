528e6d8954ae310bf651e5bffd541265056175c3
AdriM02 <adrian.las.arana@gmail.com>
2018-01-17 17:46:32 +0100

    Archivo calculadora terminado

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,13 @@
-tipo_operacion = input ("¿Qué operación quieres realizar (multiplicar / sumar / restar)?")
-primer_numero = input ("Primer número: ")
-segundo_numero = input ("Segundo número: ")
+tipo_operacion = input ("¿Qué operación quieres realizar (multiplicar / sumar / restar)?: ")
+primer_numero = int(input ("Primer número: "))
+segundo_numero = int(input ("Segundo número: "))
+resultado = str("Error")
 
-if tipo_operacion == ("multiplicar")
-    print (primer_numero * segundo_numero)
+if tipo_operacion == "multiplicar":
+    resultado = int(primer_numero * segundo_numero)
+elif tipo_operacion == "sumar":
+    resultado = int(primer_numero + segundo_numero)
+elif tipo_operacion == "restar":
+    resultado = int(primer_numero - segundo_numero)
+
+print ("Resultado: {}".format(resultado))
