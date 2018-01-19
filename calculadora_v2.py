cb106fcfbb20fdf24605a39c9cf55633f51c573d
AdriM02 <adrian.las.arana@gmail.com>
2018-01-19 21:47:17 +0100

    calculadora_v2 2.0

--- calculadora_v2.py
+++ calculadora_v2.py
@@ -2,21 +2,29 @@ tipo_operacion = None
 primer_numero = None
 segundo_numero = None
 
-while tipo_operacion or primer_numero or segundo_numero == None:
-    tipo_operacion = input ("¿Qué operación quieres realizar (multiplicar / sumar / restar)?: ")
+while tipo_operacion == None or primer_numero == None or segundo_numero == None:
+    tipo_operacion = (input("¿Qué operación quieres realizar (multiplicar / sumar / restar)?: ")).upper()
+    while tipo_operacion != ("MULTIPLICAR") and tipo_operacion != ("SUMAR") and tipo_operacion != ("RESTAR"):
+        print('Solo puedes escribir "multiplicar", "sumar" o "restar"')
+        tipo_operacion = (input("¿Qué operación quieres realizar (multiplicar / sumar / restar)?: ")).upper()
+
     primer_numero = int(input ("Primer número: "))
     segundo_numero = int(input ("Segundo número: "))
 resultado = str("Error")
 
-
-if tipo_operacion == "multiplicar":
+################## Me aburría ##################
+if tipo_operacion == "MULTIPLICAR" and segundo_numero == 5:
     resultado = int(primer_numero * segundo_numero)
-elif tipo_operacion == "sumar":
-    resultado = int(primer_numero + segundo_numero)
-elif tipo_operacion == "restar":
-    resultado = int(primer_numero - segundo_numero)
-# Me aburría:
-elif tipo_operacion == "multiplicar" and segundo_numero == 5:
     print ("Por el culo te la ...")
+    print("Resultado: {}".format(resultado))
+    exit()
+################################################
+
+elif tipo_operacion == "SUMAR":
+    resultado = int(primer_numero + segundo_numero)
+elif tipo_operacion == "RESTAR":
+    resultado = int(primer_numero - segundo_numero)
+elif tipo_operacion == "MULTIPLICAR":
+    resultado = int(primer_numero * segundo_numero)
 
 print ("Resultado: {}".format(resultado))
